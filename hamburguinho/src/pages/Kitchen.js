0e0328c7b618b53a761fe1c91acb9a4686602a0f
fernanda <ono.frnd@gmail.com>
2020-01-14 03:21:44 -0300

    status dos pedidos e modal

--- hamburguinho/src/pages/Kitchen.js
+++ hamburguinho/src/pages/Kitchen.js
@@ -7,23 +7,70 @@ function Kitchen (){
 
     useEffect(() => { 
         firebase.firestore().collection('orders').get().then((snapshot) => {
-            snapshot.docs.map((doc) => setOrders ((current) => [...current, doc.data()]))
+            snapshot.docs.map((doc) => setOrders ((current) => [...current, {data:doc.data(), key: doc._key.path.segments[doc._key.path.segments.length -1]}]))
         })
     },[]); 
 
+    const onClick = (order) => {
+        if(order.data.creationDate
+            && !order.data.preparationDate
+            && !order.data.readyDate
+            && !order.data.deliveryDate){
+            order.data.preparationDate = new Date()
+            firebase.firestore().collection('orders').doc(order.key).set({
+                preparationDate: order.data.preparationDate
+            },{merge: true});
+        }else if(order.data.creationDate
+            && order.data.preparationDate
+            && !order.data.readyDate
+            && !order.data.deliveryDate){
+            order.data.readyDate = new Date ()
+            firebase.firestore().collection('orders').doc(order.key).set({
+                readyDate: order.data.readyDate
+            },{merge: true});
+        }else if(order.data.creationDate
+            && order.data.preparationDate
+            && order.data.readyDate
+            && !order.data.deliveryDate){
+            order.data.deliveryDate = new Date ()
+            firebase.firestore().collection('orders').doc(order.key).set({
+                deliveryDate: order.data.deliveryDate
+            },{merge: true});
+        }
+        let newOrders = orders.filter((e) => e.key !== order.key)
+        setOrders([...newOrders,order])
+    }
+
     return (
         <div className='container'>
-            {orders.map((order, index) => 
-            <OrderCard creationDate = {order.creationDate}
-            key = {index}
-            time = {order.time}
-            table = {order.table}
-            name = {order.name} 
-            items = {order.items}
-            status = {order.status}
-            
-            />  
-            )}
+
+            <div className= 'creation'>
+            <h1>Criados</h1>
+                {orders.filter((e) => e.data.creationDate && !e.data.preparationDate && !e.data.readyDate && !e.data.deliveryDate).map((order)=>
+                    <OrderCard {...order.data} buttonText = 'Preparar' onClick = {() =>{onClick(order)}}
+                    />
+                )}
+            </div>
+            <div className='preparation'>
+            <h1>Preparando</h1>
+                {orders.filter((e) => e.data.creationDate && e.data.preparationDate && !e.data.readyDate && !e.data.deliveryDate).map((order)=>
+                    <OrderCard {...order.data} buttonText = 'Pronto' onClick = {() =>{onClick(order)}}
+                    />
+                )}
+            </div>
+            <div className='ready'>
+            <h1>Prontos</h1>
+                {orders.filter((e) => e.data.creationDate && e.data.preparationDate && e.data.readyDate && !e.data.deliveryDate).map((order)=>
+                    <OrderCard {...order.data} buttonText = 'Entregue' onClick = {() =>{onClick(order)}}
+                    />
+                )}
+            </div>
+            <div className='delivery'>
+            <h1>Entregue</h1>
+                {orders.filter((e) => e.data.creationDate && e.data.preparationDate && e.data.readyDate && e.data.deliveryDate).map((order)=>
+                    <OrderCard {...order.data}/>
+                )}
+            </div>
         </div>
     );
 };
--- hamburguinho/src/pages/Restaurant.js
+++ hamburguinho/src/pages/Restaurant.js
@@ -2,19 +2,60 @@ import React, {useEffect, useState, useRef} from 'react';
 import firebase from '../utils/firebaseUtils';
 import MenuCard from '../components/Menucard';
 import Receipt from '../components/Receipt';
+import Modal from '../components/Modal';
 
-function placeOrder(items, name, table){
-    if(name  == null)
+const HamburgerType = ({item, onTypeSelect}) => {
+    return (
+        <div>
+            <label> Tipo de Hamburguer:
+            {item.type.map((e, i)=>
+                <><input
+                name= 'type'
+                type= 'radio'
+                key = {i}
+                value = {e} 
+                onClick = {() => {onTypeSelect(e)}}/>{e}<br/></>
+            )}
+                </label>
+        </div>
+    );
+};
+
+const Extra = ({item, onExtraSelect}) => {
+    return (
+        <div>
+        <label> Adicional:
+        {item.extra.map((e, i)=>
+            <><input
+            name= 'extra'
+            type= 'radio'
+            key = {e.name}
+            value = {e.price} 
+            onClick = {() => {onExtraSelect(e.name)}}
+            />{e.name}<br/></>
+        )}
+            </label>
+        </div>
+    );
+};
+function placeOrder(items, name, table, onOrderPlaced){
+    if(!name && !table)
+        return alert("Preencha o nome do cliente e mesa")
+    if(!name)
         return alert("Preencha o nome do cliente")
+    if(!table)
+        return alert("Preencha o número da mesa")
 
-    firebase.firestore().collection('orders').doc().set({
+        firebase.firestore().collection('orders').doc().set({
         name: name,
         table: table,
         items: items,
-        status: 'pending',
-        time:   new Date().toLocaleString('pt-BR')
+        creationDate: new Date(),
+        preparationDate: null,
+        readyDate: null,
+        deliveryDate: null,
 
-    }).then(alert("Pedido Enviado!"));
+    }).then(() => {alert("Pedido Enviado!");onOrderPlaced()});
 }
 function Restaurant (){
     const [menu, setMenu] = useState([]);
@@ -22,6 +63,10 @@ function Restaurant (){
     const [breakfast, setBreakfast] = useState(null);
     const [name, setName] = useState(null);
     const [table, setTable] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [children, setChildren] = useState(null);
+    const [type, setType] = useState('');
+    const [extra, setExtra] = useState({price:0,name:''});
     const nameRef = useRef(null);
     const tableRef = useRef (null)
 
@@ -35,16 +80,38 @@ function Restaurant (){
     },[]); 
     
     const addOrder = (menuItem) => {
+        setChildren(null);
+        if(menuItem.type && menuItem.extra){
+            setChildren(<div><HamburgerType item = {menuItem} onTypeSelect= {onTypeSelect}/> Extra item = {menuItem} onExtraSelect = {onExtraSelect}/></div>);
+            setShowModal(true)
+        }
         setItems([...items, menuItem])   
     }
 
     const onDelete = key => {
-        setItems(items.filter((x,i) => i !== key))
+        setItems(items.filter((del,i) => i !== key))
     }
+
+    const onOrderPlaced = () => {
+        setItems([]);
+        setBreakfast(null);
+        setName(null);
+        setTable(null);
+        nameRef.current.value = ""
+        tableRef.current.value = ""
+    }
+
+    const onTypeSelect = (option) => {
+        setType(option);
+    }
+    const onExtraSelect = (option) => {
+        setExtra(option);
+    }   
 
     return(
         
         <div>
+            <Modal setShowModal = {setShowModal} show = {showModal} children = {children}/>
             <section className="btn-set" onClick={()=>{setBreakfast(true)}} >Café da Manhã</section>
             <section className="btn-set" onClick={()=>{setBreakfast(false)}}>Almoço/Jantar</section>
             <br></br>
@@ -54,19 +121,19 @@ function Restaurant (){
             )}
 
             <section>
-
-                <Receipt {...{name: name, items: items, table: table}} onDelete = {onDelete}/>
+                <Receipt {...{name: name, items: items, table: table, extra:extra, type:type}} onDelete = {onDelete}/>
                 <br></br>
-                <label>Cliente</label>
-                <input ref = {nameRef} placeholder="Nome do Cliente" onChange = {()=>{
-                    setName(nameRef.current.value)}}> 
-                </input>
-                <label>N. Mesa</label>
-                <input ref = {tableRef} placeholder="N. Mesa" onChange = {()=>{
-                    setTable(tableRef.current.value)}}>
-                </input>
-                
-                <button onClick = {()=>{placeOrder(items,name,table)}}>Enviar</button>
+                <form className="frm-container">
+                <label>Cliente </label>
+                <input className="input" type="text" ref = {nameRef} placeholder="Nome do Cliente" onChange = {()=>{
+                    setName(nameRef.current.value)}}/> 
+                <p></p>
+                <label>N. Mesa </label>
+                <input className="input" type="text" ref = {tableRef} placeholder="N. Mesa" onChange = {()=>{
+                    setTable(tableRef.current.value)}}/>
+                <p></p>
+                </form>
+               <button onClick = {()=>{placeOrder(items,name,table,onOrderPlaced)}}>Enviar</button>
             </section>
         </div>
         
